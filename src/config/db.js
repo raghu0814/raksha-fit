ec3649f5822885679c62ff7e45ac422df843470f
raghu chowdary <raghugadupudi13@gmail.com>
2026-02-11 16:50:54 +0530

    switched to DATABASE_URL for production

--- src/config/db.js
+++ src/config/db.js
@@ -1,16 +1,11 @@
-import 'dotenv/config';   // 🔥 THIS LINE FIXES EVERYTHING
-import pkg from 'pg';
-
+import pkg from "pg";
 const { Pool } = pkg;
 
-console.log('DB_PASSWORD TYPE:', typeof process.env.DB_PASSWORD);
-
 const pool = new Pool({
-  host: process.env.DB_HOST,
-  port: Number(process.env.DB_PORT),
-  database: process.env.DB_NAME,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD, // must already be string
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false,
+  },
 });
 
 export default pool;
